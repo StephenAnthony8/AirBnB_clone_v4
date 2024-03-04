aed0c3b54cdb95bc0f8c9ecbc313c7d3e83ff841
StephenAnthony8 <anthonysteve02@gmail.com>
2024-03-04 11:30:39 +0300

    finally

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,82 +1,65 @@
 /* documentation script */
-/* function placeconstruct (place) {
-  let rtnHtml;
 
-  for (x in place) {
-    eachConstruct(x).then(
-      function (value) { rtnHtml += value; }
-    );
-  }
-  $('section.places').html(rtnHtml);
-}
-async function eachConstruct (place) {
+function eachConstruct (place, userName) {
   const nArticle = $('<article></article>');
   const ndivpbn = $('<div></div>').addClass('price_by_night');
-  const userUrl = 'http://0.0.0.0:5001/api/v1/users/' + place.user_id;
 
-  const userName = $.get(userUrl, (data, status) => {
-    if (status === 'success') {
-      return (data.first_name + ' ' + data.last_name);
-    } else {
-      return ('unavailable');
-    }
+  const ndivs = ['title_box', 'information', 'user', 'description'].map((x) => {
+    return $('<div></div>').addClass(x);
+  });
+  const infodivs = {
+    max_guest: 'Guest',
+    number_rooms: 'Bedroom',
+    number_bathrooms: 'Bathroom'
+  };
+  for (const x in infodivs) {
+    const stringAmenity = place[x].toString() + ' ' + infodivs[x] + (place[x] !== 1 ? 's' : '');
+    infodivs[x] = $('<div></div>').addClass(x).html(stringAmenity);
+  }
+
+  /* 0 */
+  const usdNightPrice = '$' + place.price_by_night.toString();
+  ndivs[0].append(('<h2>' + place.name + '</h2>')).append(ndivpbn.append(usdNightPrice));
+
+  /* 1 */
+  for (const x in infodivs) {
+    ndivs[1].append(infodivs[x]);
+  }
+
+  /* 2 */
+  ndivs[2].append('<b>Owner:</b>').append(' ' + userName);
+
+  /* 3 */
+  ndivs[3].append((place.description ? place.description : 'None'));
+
+  ndivs.map(function (divitem) {
+    return (nArticle.append(divitem));
   });
 
-  const ndivs = [
-    $('<div></div>').addClass('title_box'),
-    $('<div></div>').addClass('information'),
-    $('<div></div>').addClass('user').html('<b>Owner:</b>'),
-    $('<div></div>').addClass('description')
-  ];
-  const infodivs = [
-    $('<div></div>').addClass('max_guest').html(('Guest' + (place.max_guest != 1 ? '' : 's'))),
-    $('<div></div>').addClass('number_rooms').html(('Bedroom' + (place.number_rooms != 1 ? '' : 's'))),
-    $('<div></div>').addClass('number_bathrooms').html(('Bathroom' + (place.number_bathrooms != 1 ? '' : 's')))
-  ]; */
-/* 0 */
-// ndivs[0].append(('<h2>' + place.name + '</h2>')).append(ndivpbn.append(place.price_by_night));
-
-/* 1 */
-/* infodivs.forEach(function (item) {
-    ndivs[1].append(item);
-  }); */
-
-/* 2 */
-/* ndivs[2].append(userName); */
-
-/* 3 */
-/* ndivs[3].append((place.description ? place.description : 'None')); */
-
-/* ndivs.forEach(function (divitem) {
-    nArticle.append(divitem);
-  });
   return (nArticle);
-} */
+}
 
-/* function jsonRetriever (appendUrl, json_item) {
-} */
-/* function postStatus (localUrl) {
-  const postUrl = localUrl + 'places_search/';
+const postStatus = new Promise(function (postSuccess, postFail) {
+  const postUrl = 'http://0.0.0.0:5001/api/v1/places_search/';
   const postData = JSON.stringify({});
   const postHeaders = {
     Accept: 'application/json',
     'Content-Type': 'application/json'
   };
-
   $.ajax({
     headers: postHeaders,
     type: 'POST',
     url: postUrl,
-    data: postData,
-    success: function (result) {
-      /* alert(result.length); */
-/* placeconstruct(result);
-    },
-    error: function (error) {
-      alert('error');
+    data: postData
+  }).done((data, status) => {
+    if (status === 'success') {
+      postSuccess(data);
     }
+  }).fail(() => {
+    postFail('Error encountered');
   });
-} */
+});
+
 /* get function */
 function getStatus (localUrl) {
   const urlstatus = localUrl + 'status';
@@ -95,7 +78,22 @@ $(document).ready(() => {
 
   getStatus(localUrl);
 
-  // postStatus(localUrl);
+  let namePlace = [];
+  postStatus.then((data) => {
+    namePlace = data.slice();
+
+    Promise.all(data.map((place) => {
+      const userlink = localUrl + 'users/' + place.user_id;
+      return ($.get(userlink));
+    }))
+      .then((responses) => {
+        for (const x in responses) {
+          const userName = responses[x].first_name + ' ' + responses[x].last_name;
+
+          $('section.places').append(eachConstruct(namePlace[x], userName));
+        }
+      });
+  });
 
   $('input[type=checkbox]').change(function () {
     const amenityName = $(this).data('name').slice(1);
